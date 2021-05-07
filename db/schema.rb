461d01d236a39f3aaaa6a54d4a1fb7149361de4b
“Abbie <abbie.coghlan@gmail.com>
2021-05-07 15:57:30 -0500

    add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_174839) do
+ActiveRecord::Schema.define(version: 2021_05_07_205606) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_174839) do
     t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
