1a70e84820f259ad21ba725fac4cd9a76d308099
“Abbie <abbie.coghlan@gmail.com>
2021-05-07 17:10:04 -0500

    added user emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :notes
     validates :username, uniqueness: true
     validates :username, presence: true
-    validates :email, presence: true
+    # validates :email, presence: true
     validates :password, length: { in: 6..15 }
 
     has_secure_password
